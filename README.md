be775bd358ee515c79f45262f6baa1109ec643de
Guy Brukhis <guy.br7@gmail.com>
2018-02-25 23:55:38 +0200

    change default razzle stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # TopSub
 Subscribe to search results
+
+## Install
+* `git clone git@github.com:guyb7/TopSub.git`
+* `yarn`
+* create `.env`
+
+## Development
+* `yarn start`
+
+## Tests
+`yarn test`
+
+## Deployment
--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-import App from './App';
-import React from 'react';
-import { StaticRouter } from 'react-router-dom';
-import express from 'express';
-import { renderToString } from 'react-dom/server';
+import App from './App'
+import React from 'react'
+import { StaticRouter } from 'react-router-dom'
+import express from 'express'
+import { renderToString } from 'react-dom/server'
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
-const server = express();
+const server = express()
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
-    const context = {};
+    const context = {}
     const markup = renderToString(
       <StaticRouter context={context} location={req.url}>
         <App />
       </StaticRouter>
-    );
+    )
 
     if (context.url) {
-      res.redirect(context.url);
+      res.redirect(context.url)
     } else {
       res.status(200).send(
         `<!doctype html>
@@ -27,7 +27,7 @@ server
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
+        <title>Welcome to TopSub</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${assets.client.css
           ? `<link rel="stylesheet" href="${assets.client.css}">`
@@ -40,8 +40,8 @@ server
         <div id="root">${markup}</div>
     </body>
 </html>`
-      );
+      )
     }
-  });
+  })
 
-export default server;
+export default server
