d57231193e63ca282606e6d9f89996eeba165c09
Guy Brukhis <guy.br7@gmail.com>
2018-03-11 03:07:19 +0200

    style hn component

--- src/api/controllers/Topics/HackerNews/index.js
+++ src/api/controllers/Topics/HackerNews/index.js
@@ -61,7 +61,7 @@ class HackerNews extends BaseTopic {
   }
 
   extractDomain(url) {
-    const matches = url.match(/^https?:\/\/([^/?#]+)(?:[/?#]|$)/i)
+    const matches = url.match(/^https?:\/\/(?:www\.)?([^/:?#]+)(?:[/:?#]|$)/i)
     const domain = matches && matches[1]
     return domain
   }
--- src/components/Search/components/HackerNews.js
+++ src/components/Search/components/HackerNews.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import { withStyles } from 'material-ui/styles'
-
-import { ListItem, ListItemText } from 'material-ui/List'
+import moment from 'moment'
 
 const styles = theme => {
   return {
     root: {
-      '&:not(:last-child)': {
-        borderBottom: '1px solid ' + theme.palette.divider
+      backgroundColor: '#f6f6ef',
+      color: '#000',
+      width: '100%',
+      padding: 5,
+      paddingLeft: 10,
+      paddingRight: 10,
+      '&:first-child': {
+        paddingTop: 10
+      },
+      '&:last-child': {
+        paddingBottom: 10
+      },
+      '& a': {
+        color: '#000',
+        textDecoration: 'none'
+      },
+      '& a:visited': {
+        color: '#828282'
+      }
+    },
+    title: {
+      fontSize: 14,
+      paddingTop: 2,
+      paddingBottom: 2
+    },
+    domain: {
+      marginLeft: 4
+    },
+    subtext: {
+      color: '#828282',
+      fontSize: 11,
+      '& a': {
+        color: '#828282',
+      },
+      '& a:hover': {
+        textDecoration: 'underline'
       }
     }
   }
@@ -16,13 +49,26 @@ const styles = theme => {
 class HackerNews extends React.Component {
   render() {
     const { classes, data } = this.props
+    const timeAgo = moment.utc(data.publishTime).fromNow()
+    const hnUrl = `https://news.ycombinator.com/item?id=${data.externalId}`
+    
     return (
-      <ListItem classes={classes} button component='a' href={data.url} target='_blank'>
-        <div>
-          {data.score}
+      <div className={classes.root}>
+        <div className={classes.title}>
+          <a href={data.url} target='_blank'>
+            {data.data.title}
+          </a>
+          {
+            data.data.domain &&
+            <span className={`${classes.subtext} ${classes.domain}`}>
+              ({data.data.domain})
+            </span>
+          }
         </div>
-        <ListItemText primary={data.data.title} secondary={data.data.domain} />
-      </ListItem>
+        <div className={classes.subtext}>
+          {data.score} points <a href={hnUrl} target='_blank'>{timeAgo}</a> | <a href={hnUrl} target='_blank'>{data.data.comments} comments</a>
+        </div>
+      </div>
     )
   }
 }
