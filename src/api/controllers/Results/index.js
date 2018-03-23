8123dc28a81bdbcaf35af3bd9bb5b221c7565fce
Guy Brukhis <guy.br7@gmail.com>
2018-03-23 02:40:38 +0200

    refactor search results

---
+++ src/api/controllers/Results/index.js
@@ -0,0 +1,45 @@
+import moment from 'moment'
+
+import DB from '../../../DB/'
+
+const getStartTime = period => {
+  let since
+  switch (period) {
+    case '3days':
+      since = moment().utc().startOf('day').subtract(3, 'days')
+      break
+    case 'hour':
+    case 'day':
+    case 'week':
+    case 'month':
+      since = moment().utc().startOf(period)
+      break
+    default:
+      since = moment().utc().startOf('day')
+  }
+  since = since.format('YYYY-MM-DD HH:mm:ss+0000')
+  return since
+}
+
+const find = async ({ limit, topic, period }) => {
+  const since = getStartTime(period)
+  const Op = DB.sequelize.Op
+  const results = await DB.models.Results.findAll({
+    where: {
+      topic,
+      publishTime: {
+        [Op.gte]: since
+      }
+    },
+    order: [
+      ['score', 'DESC']
+    ],
+    limit,
+    attributes: { exclude: ['createdAt'] }
+  })
+  return results
+}
+
+export default {
+  find
+}
--- src/api/routes/getSearch.js
+++ src/api/routes/getSearch.js
@@ -1,45 +1,10 @@
-import moment from 'moment'
-
-import DB from '../../DB/'
+import Results from '../controllers/Results/'
 import Topics from '../controllers/Topics/'
-
-const getStartTime = period => {
-  let since
-  switch (period) {
-    case '3days':
-      since = moment().utc().startOf('day').subtract(3, 'days')
-      break
-    case 'hour':
-    case 'day':
-    case 'week':
-    case 'month':
-      since = moment().utc().startOf(period)
-      break
-    default:
-      since = moment().utc().startOf('day')
-  }
-  since = since.format('YYYY-MM-DD HH:mm:ss+0000')
-  return since
-}
 
 export default async req => {
   const { limit, topic, period } = req.query
   const component = (Topics.topicsDict[topic] && Topics.topicsDict[topic].info().component) || 'basic'
-  const since = getStartTime(period)
-  const Op = DB.sequelize.Op
-  const results = await DB.models.Results.findAll({
-    where: {
-      topic,
-      publishTime: {
-        [Op.gte]: since
-      }
-    },
-    order: [
-      ['score', 'DESC']
-    ],
-    limit,
-    attributes: { exclude: ['createdAt'] }
-  })
+  const results = await Results.find({ limit, topic, period })
   return {
     success: true,
     component,
